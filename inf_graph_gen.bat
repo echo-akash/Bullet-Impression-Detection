11012413be554ae6e5b2a79751ca5ebfa86f187f
akash poddar <akash.poddar.0799@gmail.com>
2019-10-10 21:56:07 +0600

    Add files via upload

---
+++ inf_graph_gen.bat
@@ -0,0 +1,2 @@
+rmdir /s "C:\TensorFlow\research\object_detection\inference_graph"
+python export_inference_graph.py --input_type image_tensor --pipeline_config_path training/faster_rcnn_inception_v2_pets.config --trained_checkpoint_prefix training/model.ckpt-52852 --output_directory inference_graph
