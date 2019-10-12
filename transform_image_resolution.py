72c6d8f25219875a096c08e879b7b52fdb46d779
akash poddar <akash.poddar.0799@gmail.com>
2019-10-12 23:03:19 +0600

    Add files via upload

---
+++ transform_image_resolution.py
@@ -0,0 +1,17 @@
+#To run as script type following in the parent directory of the images
+#python transform_image_resolution.py -d folder_name/ -s new_resolution_height new_resolution_width
+#example-> python transform_image_resolution.py -d images/ -s 800 600
+
+
+from PIL import Image
+import os
+import argparsedef rescale_images(directory, size):
+    for img in os.listdir(directory):
+        im = Image.open(directory+img)
+        im_resized = im.resize(size, Image.ANTIALIAS)
+        im_resized.save(directory+img)if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Rescale images")
+    parser.add_argument('-d', '--directory', type=str, required=True, help='Directory containing the images')
+    parser.add_argument('-s', '--size', type=int, nargs=2, required=True, metavar=('width', 'height'), help='Image size')
+    args = parser.parse_args()
+    rescale_images(args.directory, args.size)
